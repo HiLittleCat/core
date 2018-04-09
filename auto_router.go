3f3bfc8e03d48e36bd3f2376ca34f8efd194e092
realya_xupengfei <xpf@realya.cn>
2018-04-09 18:11:38 +0800

    自动路由拆为单独文件

---
+++ auto_router.go
@@ -0,0 +1,73 @@
+package core
+
+import (
+	"net/http"
+	"reflect"
+	"strings"
+)
+
+var mRoutering = make(map[string]reflect.Type)
+
+// AutoController register controller
+func AutoController(controller IController) {
+	refV := reflect.ValueOf(controller)
+	refT := reflect.Indirect(refV).Type()
+	refName := strings.ToLower(refT.Name())
+	mRoutering[strings.ToLower(refName)] = refT
+}
+
+const (
+	defController = "index"
+	defMethod     = "Index"
+)
+
+func findControllerInfo(r *http.Request) (string, string) {
+	path := r.URL.Path
+	path = strings.TrimSuffix(path, "/")
+	pathInfo := strings.Split(path, "/")
+
+	controllerName := defController
+	if len(pathInfo) > 0 {
+		controllerName = strings.ToLower(pathInfo[1])
+	}
+	methodName := defMethod
+	if len(pathInfo) == 2 {
+		methodName = strings.Title(strings.ToLower(r.Method))
+	} else if len(pathInfo) == 3 {
+		methodName = strings.Title(strings.ToLower(r.Method) + pathInfo[2])
+	}
+	return controllerName, methodName
+}
+
+// AutoRouter route middleware
+func AutoRouter(ctx *Context) {
+	r := ctx.Request
+	controllerName, methodName := findControllerInfo(r)
+	controllerT, ok := mRoutering[controllerName]
+	if !ok {
+		ctx.Fail(http.StatusNotFound, &NotFoundError{Message: "Module Not Found"})
+		return
+	}
+	refV := reflect.New(controllerT)
+	method := refV.MethodByName(methodName)
+	if !method.IsValid() {
+		ctx.Fail(http.StatusNotFound, &NotFoundError{Message: "Method Not Found"})
+		return
+	}
+	ctx.Data["Controller"] = controllerName
+	ctx.Data["Method"] = methodName
+	controller := refV.Interface().(IController)
+	controller.Init(ctx)
+	v := method.Call(nil)
+	if len(v) == 0 {
+		return
+	}
+	value := v[0].Interface()
+	err := v[1].Interface()
+	if err != nil {
+		ctx.Fail(http.StatusInternalServerError, &ServerError{Message: err.(error).Error()})
+		return
+	}
+	//ctx.Data["ResData"] = value
+	ctx.Ok(http.StatusOK, value)
+}
